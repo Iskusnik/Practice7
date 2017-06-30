19e03cc6e7efee13440ae5a452f9cd630679a4b4
IskusnikXD <IskusnikXD@ISKUSNIK>
2017-06-30 23:24:53 +0500

    Необходимо построить дерево: -В разработке класс - вершина дерева для создания языка

--- Practice7/Program.cs
+++ Practice7/Program.cs
@@ -8,8 +8,61 @@ namespace Practice7
 {
     class Program
     {
+        /// <summary>
+        /// 21.	
+        /// Заданы частоты символов входного алфавита. 
+        ///        -на вход - N - число символов
+        ///        -N частот words символов, частоты задаём целым числом - количество встреченных символов в тексте
+        ///     
+        /// Построить двоичный суффиксный код Хаффмана. 
+        ///     -любое слово не суффикс другого
+        ///     -строим дерево для префиксного кода и собираем снизу 
+        ///     
+        /// Кодовые слова выписать в лексикографическом порядке.
+        ///  00, 01, 010 и тд
+        /// </summary>
+        /// <param name="args"></param>
+        /// 
+
+        class Node
+        {
+            Node L;         //Левая вершина
+            Node R;         //Правая вершина
+            int Index;      //Индекс в массиве, если вершина конечная
+            int Sum;        //Сумма частот левой и правой вершины
+            public Node()
+            {
+                L = null;
+                R = null;
+                Index = -1;
+                Sum = 0;
+            }
+            public Node(Node l, Node r)
+            {
+                L = l;
+                R = r;
+                Index = -1;
+                Sum = l.Sum + r.Sum;
+            }
+            public Node(int index, int sum)
+            {
+                L = null;
+                R = null;
+                Index = index;
+                Sum = sum;
+            }
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число символов исходного алфавита");
+            int N = int.Parse(Console.ReadLine());
+            int[] words = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("Введите сколько раз встретилось символ {0}", i + 1);
+                words[i] = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
